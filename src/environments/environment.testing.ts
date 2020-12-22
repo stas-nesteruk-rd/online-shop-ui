8158939c6a48ac061cbcc652d898cb58952bd21f
Stas Neteruk <cognes.com@gmail.com>
2020-12-22 15:58:09 +0300

    feat(221220): add test environment

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,8 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyA_AuQh96RfbevYQl_GED5bbLYx0G1gz88',
+  dbUrl: 'https://online-store-test-65b00-default-rtdb.firebaseio.com',
+  apiUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
+};
